42d67c37799cd5e593c6caa0d44823af198e736b
icemokacat <icemokacat@gmail.com>
2024-08-23 12:53:42 +0900

    feat: 프로젝트 최초등록

---
+++ src/main/java/moka/brunoviewer/BrunoviewerApplication.java
@@ -0,0 +1,13 @@
+package moka.brunoviewer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BrunoviewerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BrunoviewerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/moka/brunoviewer/BrunoviewerApplicationTests.java
@@ -0,0 +1,13 @@
+package moka.brunoviewer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BrunoviewerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
