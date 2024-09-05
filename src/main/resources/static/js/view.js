414ecf34590542d932e73e9459dfe3efb1342a8e
icemokacat <icemokacat@gmail.com>
2024-09-05 16:11:18 +0900

    🔥 fix: 마크다운 view 렌더링시 js 오류 수정
    - 제목 가져오는 스크립트 오류 수정

--- src/main/resources/static/js/view.js
+++ src/main/resources/static/js/view.js
@@ -4,7 +4,7 @@ const mdViewer = {
     action : {
         setTitle : function () {
             // article 내 최초 h1의 텍스트를 title로 설정
-            const titleText = document.querySelector('article h1').innerText;
+            const titleText = document.querySelector('article h2').innerText;
             if (titleText) {
                 document.title = titleText;
             }
